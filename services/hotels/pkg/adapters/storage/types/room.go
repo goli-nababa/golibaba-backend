2bf32ccdadce9a2d79e131e7f46e1b8e3f4c229c
Shahryar <dev.shahryar@gmail.com>
2024-12-22 19:32:57 +0330

    fix(pkg/storage): fix types name

---
+++ services/hotels/pkg/adapters/storage/types/room.go
@@ -0,0 +1,16 @@
+package types
+
+import "time"
+
+type Room struct {
+	ID         string
+	HotelID    string
+	RateID     string
+	RoomNumber string
+	Capacity   uint
+	Features   []string
+	IsAvilabe  bool
+	CreateAt   time.Time
+	UpdateAt   time.Time
+	DeletedAt  time.Time
+}
