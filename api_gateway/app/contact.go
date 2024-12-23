d6fe86924f44bb708d1c60bed676dbb19cb30f7d
Hossein Araghi <hoseinaraghi84@gmail.com>
2024-12-23 19:45:30 +0330

    feat(gateway): Inject cache module to app

--- api_gateway/app/contact.go
+++ api_gateway/app/contact.go
@@ -1,7 +1,11 @@
 package app
 
-import "api_gateway/config"
+import (
+	"api_gateway/config"
+	"github.com/goli-nababa/golibaba-backend/modules/cache"
+)
 
 type App interface {
 	Config() config.Config
+	Cache() cache.Provider
 }
