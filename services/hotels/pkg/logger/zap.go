fd20e4d9e558a6f862d8aa4083eea5b341c617df
Shahryar <dev.shahryar@gmail.com>
2024-12-24 13:50:00 +0330

    fix(pkg/logger): change logger structure

--- services/hotels/pkg/logger/zap.go
+++ services/hotels/pkg/logger/zap.go
@@ -1,47 +1,15 @@
 package logger
 
 import (
-	"sync"
-
 	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
 )
 
-var (
-	log  *zap.Logger
-	once sync.Once
-)
-
-func NewLogger(level zapcore.Level, isProduction bool) {
-	once.Do(func() {
-		var config zap.Config
-		if isProduction {
-			config = zap.NewProductionConfig()
-		} else {
-			config = zap.NewDevelopmentConfig()
-		}
-
-		config.Level.SetLevel(level)
-
-		var err error
-
-		log, err = config.Build()
-		if err != nil {
-			panic(err)
-		}
-
-	})
-}
-
-func GetLogger() *zap.Logger {
-	if log == nil {
-		NewLogger(zap.InfoLevel, true)
+func Logger() *zap.Logger {
+	config := zap.Config{
+		Level:       zap.NewAtomicLevelAt(zap.InfoLevel),
+		Development: false,
+		Sampling:    nil,
 	}
-	return log
-}
 
-func Sync() {
-	if log != nil {
-		_ = log.Sync()
-	}
+	return zap.Must(config.Build())
 }
